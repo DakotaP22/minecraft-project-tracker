ab434070b1b26ce12794da98cd9e3ccc635d7347
Dakota Pease <peased22@gmail.com>
2022-09-29 15:17:04 -0400

    adds user service + registration functionality

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { User, Admin, Record } from 'pocketbase';
+import { BehaviorSubject } from 'rxjs';
+import { UserService } from './core/user-service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ui';
+  user$: BehaviorSubject<any>;
+
+  constructor(private userSvc: UserService) {
+    this.user$ = userSvc.user$;
+
+    this.user$.subscribe((user: User | Admin | null) => {
+      console.log('user', user);
+    });
+  }
+
+  displayUserName(user: User | Admin | null) {
+    if (!user) return undefined;
+    else if (user instanceof User) {
+      return user.profile ? user.profile['username'] : undefined;
+    } else {
+      return 'Admin';
+    }
+  }
 }
--- ui/src/app/pages/registration-page/registration-page.component.ts
+++ ui/src/app/pages/registration-page/registration-page.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { Validators } from '@angular/forms';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 import { RegistrationCarouselComponent } from './components/registration-carousel/registration-carousel.component';
+import { UserService } from 'src/app/core/user-service/user.service';
 
 @Component({
   standalone: true,
@@ -20,11 +21,24 @@ import { RegistrationCarouselComponent } from './components/registration-carouse
   styleUrls: ['./registration-page.component.scss'],
 })
 export class RegistrationPageComponent implements OnInit {
-  constructor() {}
+  constructor(private userSvc: UserService) {}
 
   ngOnInit(): void {}
 
-  onSubmit(event: unknown) {
+  onSubmit(event: {
+    email: string;
+    username: string;
+    password: string;
+    confirmPassword: string;
+  }) {
     console.log(event);
+    this.userSvc
+      .registerUser(
+        event.username,
+        event.email,
+        event.password,
+        event.confirmPassword
+      )
+      .then(console.log);
   }
 }
